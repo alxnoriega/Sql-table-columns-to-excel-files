163a3482aeb2c1d8f5fd0f0bddd2a69e2ac0a2e0
alxnoriega <noriega.alex@gmail.com>
2014-06-27 09:38:01 -0700

    Test

---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectarDB = new System.Windows.Forms.Button();
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.dgvTables = new System.Windows.Forms.DataGridView();
+            this.btnCrearDocs = new System.Windows.Forms.Button();
+            this.TABLE_NAME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columnas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chkDoc = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.txtOutPutFolder = new System.Windows.Forms.TextBox();
+            this.btnOutPutFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTables)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConectarDB
+            // 
+            this.btnConectarDB.Location = new System.Drawing.Point(430, 36);
+            this.btnConectarDB.Name = "btnConectarDB";
+            this.btnConectarDB.Size = new System.Drawing.Size(119, 23);
+            this.btnConectarDB.TabIndex = 0;
+            this.btnConectarDB.Text = "Conectar a DB";
+            this.btnConectarDB.UseVisualStyleBackColor = true;
+            this.btnConectarDB.Click += new System.EventHandler(this.btnConectarDB_Click);
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Location = new System.Drawing.Point(36, 38);
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(369, 20);
+            this.txtConnectionString.TabIndex = 1;
+            // 
+            // dgvTables
+            // 
+            this.dgvTables.AllowUserToDeleteRows = false;
+            this.dgvTables.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTables.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TABLE_NAME,
+            this.Columnas,
+            this.chkDoc});
+            this.dgvTables.Location = new System.Drawing.Point(12, 103);
+            this.dgvTables.Name = "dgvTables";
+            this.dgvTables.Size = new System.Drawing.Size(722, 174);
+            this.dgvTables.TabIndex = 2;
+            // 
+            // btnCrearDocs
+            // 
+            this.btnCrearDocs.Location = new System.Drawing.Point(568, 35);
+            this.btnCrearDocs.Name = "btnCrearDocs";
+            this.btnCrearDocs.Size = new System.Drawing.Size(119, 23);
+            this.btnCrearDocs.TabIndex = 3;
+            this.btnCrearDocs.Text = "Crear Documento(s)";
+            this.btnCrearDocs.UseVisualStyleBackColor = true;
+            this.btnCrearDocs.Click += new System.EventHandler(this.btnCrearDocs_Click);
+            // 
+            // TABLE_NAME
+            // 
+            this.TABLE_NAME.HeaderText = "Tablas";
+            this.TABLE_NAME.Name = "TABLE_NAME";
+            this.TABLE_NAME.ReadOnly = true;
+            // 
+            // Columnas
+            // 
+            this.Columnas.HeaderText = "Columnas";
+            this.Columnas.Name = "Columnas";
+            this.Columnas.ReadOnly = true;
+            // 
+            // chkDoc
+            // 
+            this.chkDoc.FalseValue = "False";
+            this.chkDoc.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.chkDoc.HeaderText = "Crear Doc";
+            this.chkDoc.IndeterminateValue = "False";
+            this.chkDoc.Name = "chkDoc";
+            this.chkDoc.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.chkDoc.TrueValue = "True";
+            // 
+            // txtOutPutFolder
+            // 
+            this.txtOutPutFolder.Enabled = false;
+            this.txtOutPutFolder.Location = new System.Drawing.Point(36, 65);
+            this.txtOutPutFolder.Name = "txtOutPutFolder";
+            this.txtOutPutFolder.Size = new System.Drawing.Size(369, 20);
+            this.txtOutPutFolder.TabIndex = 4;
+            // 
+            // btnOutPutFolder
+            // 
+            this.btnOutPutFolder.Location = new System.Drawing.Point(430, 65);
+            this.btnOutPutFolder.Name = "btnOutPutFolder";
+            this.btnOutPutFolder.Size = new System.Drawing.Size(119, 23);
+            this.btnOutPutFolder.TabIndex = 5;
+            this.btnOutPutFolder.Text = "Folder de Salida";
+            this.btnOutPutFolder.UseVisualStyleBackColor = true;
+            this.btnOutPutFolder.Click += new System.EventHandler(this.btnOutPutFolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 289);
+            this.Controls.Add(this.btnOutPutFolder);
+            this.Controls.Add(this.txtOutPutFolder);
+            this.Controls.Add(this.btnCrearDocs);
+            this.Controls.Add(this.dgvTables);
+            this.Controls.Add(this.txtConnectionString);
+            this.Controls.Add(this.btnConectarDB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTables)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectarDB;
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.DataGridView dgvTables;
+        private System.Windows.Forms.Button btnCrearDocs;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TABLE_NAME;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columnas;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn chkDoc;
+        private System.Windows.Forms.TextBox txtOutPutFolder;
+        private System.Windows.Forms.Button btnOutPutFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+          
+        }
+
+        private void btnConectarDB_Click(object sender, EventArgs e)
+        {
+            dgvTables.DataSource = null; 
+            ConectarADB();
+        }
+
+         void ConectarADB()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(txtConnectionString.Text))
+                {
+                    connection.Open();
+                    DataTable schema = connection.GetSchema("Tables");
+
+                    DataColumnCollection colection = connection.GetSchema("Tables").Columns;
+
+                    foreach (DataRow row in schema.Rows)
+                    {
+                        DataTable schemaColumns = connection.GetSchema("Columns", new[] { connection.Database, null, row["TABLE_NAME"].ToString() });
+                        string columnas = string.Empty;
+
+                        foreach (DataRow row2 in schemaColumns.Rows)
+                            columnas += row2["COLUMN_NAME"].ToString() + ",";
+
+                        dgvTables.Rows.Add(new object[] { row["TABLE_NAME"].ToString(), columnas, false });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+         
+        }
+
+         private void btnCrearDocs_Click(object sender, EventArgs e)
+         {
+
+             foreach (DataGridViewRow row in dgvTables.Rows)
+             {
+               
+               //int value =  int.Parse(row.Cells[1].Value.ToString());
+
+                 if (!row.IsNewRow)
+                 {
+                     bool isChecked = bool.Parse(row.Cells[2].Value.ToString());
+
+                     if (isChecked)
+                         CreateDoc(row.Cells[0].Value.ToString(),row.Cells[1].Value.ToString());
+                     else
+                         continue;
+                 }
+             }
+
+             MessageBox.Show("Finalizo exitosamente");
+         }
+
+         void CreateDoc(string tableName,string celdas)
+         {
+             Microsoft.Office.Interop.Excel.Application excel;
+             Microsoft.Office.Interop.Excel.Workbook excelworkBook;
+             Microsoft.Office.Interop.Excel.Worksheet excelSheet;
+             Microsoft.Office.Interop.Excel.Range excelCellrange;
+
+             // Start Excel and get Application object.
+             excel = new Microsoft.Office.Interop.Excel.Application();
+
+             // for making Excel visible
+             excel.Visible = false;
+             excel.DisplayAlerts = false;
+
+             // Creation a new Workbook
+             excelworkBook = excel.Workbooks.Add(Type.Missing);
+
+             // Workk sheet
+             excelSheet = (Microsoft.Office.Interop.Excel.Worksheet)excelworkBook.ActiveSheet;
+             excelSheet.Name = "tabla";
+
+             int columnCount = celdas.Split(',').Count();
+             int i = 1;
+            foreach (string columna in celdas.Split(','))
+            {
+                excelSheet.Cells[1, i] = columna;
+                excelCellrange = excelSheet.Range[excelSheet.Cells[1, 1], excelSheet.Cells[i, columnCount]];
+                excelCellrange.EntireColumn.AutoFit();
+                i++;
+            }
+
+             excelworkBook.SaveAs(txtOutPutFolder.Text + "\\"+ tableName + ".xls");
+             excel.Quit();
+
+
+             //using (SqlConnection connection = new SqlConnection(txtConnectionString.Text))
+             //{
+             //    connection.Open();
+             //    DataTable schema = connection.GetSchema("Tables");
+
+
+             //    foreach (DataRow row in schema.Rows)
+             //    {
+             //        dgvTables.Rows.Add(new object[] { row["TABLE_NAME"].ToString(), false });
+             //    }
+
+             //}
+
+
+         }
+
+         private void btnOutPutFolder_Click(object sender, EventArgs e)
+         {
+             DialogResult resultado =  folderBrowserDialog1.ShowDialog();
+
+
+             if (resultado == System.Windows.Forms.DialogResult.OK)
+                 txtOutPutFolder.Text = folderBrowserDialog1.SelectedPath;
+         }
+
+        
+    }
+}
